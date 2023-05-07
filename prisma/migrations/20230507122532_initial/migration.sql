c121b0d6be0f99e3a4e4e4db38aab807c62d85bf
Marcus Bondezan <marcus.bondezan@remessaonline.com.br>
2023-05-07 10:15:36 -0300

    feat: added freight and some validations to checkout

--- prisma/migrations/20230507122532_initial/migration.sql
+++ prisma/migrations/20230507122532_initial/migration.sql
@@ -3,6 +3,10 @@ CREATE TABLE "product" (
     "id" SERIAL NOT NULL,
     "description" TEXT NOT NULL,
     "price" DECIMAL(65,30) NOT NULL,
+    "width" INTEGER NOT NULL,
+    "height" INTEGER NOT NULL,
+    "length" INTEGER NOT NULL,
+    "weight" DECIMAL(65,30) NOT NULL,
 
     CONSTRAINT "product_pkey" PRIMARY KEY ("id")
 );
@@ -11,6 +15,7 @@ CREATE TABLE "product" (
 CREATE TABLE "coupon" (
     "code" TEXT NOT NULL,
     "percentage" DECIMAL(65,30) NOT NULL,
+    "expire_date" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "coupon_pkey" PRIMARY KEY ("code")
 );
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,17 +6,49 @@ async function main() {
   
   const products = await prisma.product.createMany({
     data: [
-      { description: 'A', price: 1000 },
-      { description: 'B', price: 5000 },
-      { description: 'C', price: 30 },
+      { 
+        id: 1, 
+        description: 'A', 
+        price: 1000,
+        width: 100,
+        height: 30,
+        length: 10,
+        weight: 3
+      },
+      { 
+        id: 2, 
+        description: 'B', 
+        price: 5000,
+        width: 50,
+        height: 50,
+        length: 50,
+        weight: 22
+      },
+      { 
+        id: 3, 
+        description: 'C', 
+        price: 30,
+        width: 10,
+        height: 10,
+        length: 10,
+        weight: 0.9
+      },
     ]
   });
 
-  const coupon = await prisma.coupon.create({
-    data: {
-      code: 'VALE20', 
-      percentage: 20
-    }
+  const coupon = await prisma.coupon.createMany({
+    data: [
+      {
+        code: 'VALE20', 
+        percentage: 20,
+        expire_date: new Date('2023-10-01T10:00:00')
+      },
+      {
+        code: 'VALE10', 
+        percentage: 10,
+        expire_date: new Date('2022-10-01T10:00:00')
+      }
+    ]
   });
 
   console.log({ products, coupon });
