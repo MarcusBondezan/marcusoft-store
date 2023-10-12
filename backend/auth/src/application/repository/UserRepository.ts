a4d3180f47fbe706c34237d8fcd9655d084ea08a
Marcus Bondezan <marcus.bondezan@remessaonline.com.br>
2023-10-12 10:25:37 -0300

    refactor (Signup): segregating interfaces example

--- backend/auth/src/application/repository/UserRepository.ts
+++ backend/auth/src/application/repository/UserRepository.ts
@@ -1,6 +1,7 @@
 import User from "../../domain/entity/User";
+import SignupUserRepository from "./SignupUserRepository";
 
-export default interface UserRepository {
+export default interface UserRepository extends SignupUserRepository {
   save (user: User): Promise<void>;
   get (email: string): Promise<User>;
   delete (email: string): Promise<void>;
--- backend/auth/src/application/usecase/Signup.ts
+++ backend/auth/src/application/usecase/Signup.ts
@@ -1,8 +1,8 @@
-import UserRepository from '../repository/UserRepository';
 import User from '../../domain/entity/User';
+import SignupUserRepository from '../repository/SignupUserRepository';
 
 export default class Signup {
-  constructor(readonly userRepository: UserRepository){}
+  constructor(readonly userRepository: SignupUserRepository){}
 
   async execute(input: Input): Promise<void> {
       const user = User.create(input.email, input.password, 'pbkdf2');
